b0ca6f818bd2b4a5247f9e19318553ac4c0a5fee
Carlos Aguilar <caguilar@dimar.codaltec.com>
2021-05-17 21:17:49 -0500

    Fixed pipeline

--- src/models/videoGame.model.ts
+++ src/models/videoGame.model.ts
@@ -1,8 +1,3 @@
-/*
-Los datos interesantes para la empresa son (con un ej): 
-|NOMBRE|CONSOLA|FECHA-INGRESO|FECHA-EGRESO|FECHA-LANZAMIENTO|DESCRIPCION
-|pokemon espada|switch|02-02-2020||15-11-2019|Nuevo juego de pokemon al estilo tradicional
-*/
 export interface IVideoGame {
     serialNumber: string;
     name: string;
